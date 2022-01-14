5c0365bc94c6dd743b565bcc4212c2fb850bdceb
Yuri Gribov <tetra2005@gmail.com>
2022-01-15 00:19:57 +0300

    Added ability to run with custom python version.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -15,5 +15,21 @@ fi
 
 cd $(dirname $0)/..
 
+# Run all child scripts via $PYTHON
+if test -n "${PYTHON:-}"; then
+  mkdir -p tmp
+  # Handle multiple args
+  set -- $PYTHON
+  exe=$(which $1)
+  shift
+  cat > tmp/python3 <<EOF
+#!/bin/sh
+$exe $@ "\$@"
+EOF
+  chmod +x tmp/python3
+  export PYTHON=python3
+  export PATH=$PWD/tmp:$PATH
+fi
+
 make
 make test
